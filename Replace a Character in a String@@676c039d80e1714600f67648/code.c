fa0f6ab8b44142945129fa2c10eaeafe1014bde3
Lakshay Mittal <lakshaymittal1031@gmail.com>
2025-04-13 00:58:30 +0530

    Solution to Replace a Character in a String

--- Replace a Character in a String@@676c039d80e1714600f67648/code.c
+++ Replace a Character in a String@@676c039d80e1714600f67648/code.c
@@ -1,20 +1,27 @@
-#include<stdio.h>
-int main(){
-    char str[50];
-    char n;
-    char a;
-    scanf("%[^\n]s",str);
-    getchar();
-    scanf("%c\n",&n);
-    scanf("%c",&a);
-    int i=0,j=0,k=0,l=0,m=0;
-    for(i=0;str[i]!='\0';i++){
-        if(str[i]==n){
-            str[i]=a;
-        }
-    }
-    for(i=0;str[i]!='\0';i++){
-        printf("%c",str[i]);
-    }
-    return 0;
-}
+// #include<stdio.h>
+// int main(){
+//     char str[50];
+//     char n;
+//     char a;
+//     scanf("%[^\n]s",str);
+//     getchar();
+//     scanf("%c\n",&n);
+//     scanf("%c",&a);
+//     int i=0,j=0,k=0,l=0,m=0;
+//     for(i=0;str[i]!='\0';i++){
+//         if(str[i]==n){
+//             str[i]=a;
+//         }
+//     }
+//     for(i=0;str[i]!='\0';i++){
+//         printf("%c",str[i]);
+//     }
+//     return 0;
+// }
+#include <stdio.h>
+#include <string.h>
+int i,j,n;
+char name[100];
+char ch;
+scanf("%s",name);
+scanf("%c",ch);
